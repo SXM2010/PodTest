0d417a01a86563d68db3e3d01f9e631788d1e484
SXM2010 <492105881@qq.com>
2015-09-23 19:56:33 +0800

    测试

---
+++ SXMSlidList/SXMSlidList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SXMSlidList
+//
+//  Created by tanlu on 15/9/23.
+//  Copyright © 2015年 tanlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SXMSlidList/SXMSlidList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXMSlidList
+//
+//  Created by tanlu on 15/9/23.
+//  Copyright © 2015年 tanlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
